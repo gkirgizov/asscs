1c03207015636303619dbd6a130280d8403ecad3
cheklmn <cheklmn9696@gmail.com>
2019-06-13 21:45:46 +0300

    Resource query update
    added new variables, constructor from ResultSet

--- src/sample/ResourceQuery.kt
+++ src/sample/ResourceQuery.kt
@@ -1,7 +1,11 @@
 package sample
 
+import java.sql.Date
+import java.sql.ResultSet;
 
-data class ResourceQuery(val id: Int, val state: State = State.Created) {
+data class ResourceQuery(val id: Int = 0, val pr: Priority = Priority.Normal, val hydrogen: Int = 0, val iron: Int = 0,
+                         val special: String = "", val preferred: Date, val dl: Date,
+                         val state: State = State.Created, val update: Date) {
 
 
     // Ну это пока примерно...
@@ -15,4 +19,33 @@ data class ResourceQuery(val id: Int, val state: State = State.Created) {
         Declined
     }
 
+    enum class Priority {
+        Normal,
+        High
+    }
+
+    var priority : Priority = pr
+        get() = Priority.valueOf(priority.toString())
+
+    var hydrogenCount: Int = hydrogen
+
+    var ironCount: Int = 0
+
+    var specialSupplies: String = special
+
+    var preferredDate: Date = preferred
+
+    var deadline: Date = dl
+
+    var status : State = state
+
+    var updated: Date = update
+
+    //Make a query out of ResultSet from DB
+    constructor(rs: ResultSet): this(rs.getInt("id"), Priority.valueOf(rs.getString("Priority")),
+            rs.getInt("Hydrogen count"), rs.getInt("Iron count"), rs.getString("Special supplies"),
+            rs.getDate("Preferred deadline"), rs.getDate("Last deadline"), State.valueOf(rs.getString("Status")),
+            rs.getDate("Created")){  }
+
+    //var
 }
